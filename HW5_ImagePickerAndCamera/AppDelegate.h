683bd9524b03712d1376bcf12d50766b5f856e00
keren man <bebemau@gmail.com>
2015-05-13 19:16:58 -0700

    Initial Commit

---
+++ HW5_ImagePickerAndCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW5_ImagePickerAndCamera
+//
+//  Created by Man, Keren on 5/13/15.
+//  Copyright (c) 2015 Man, Keren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
